392bdc7fe8e54dcde495d0b7197ef347b8492184
Jan <janek0619@mail.com>
2022-04-14 16:36:56 +0200

    feature|pisze lala

--- src/zajecia12/Main.java
+++ src/zajecia12/Main.java
@@ -2,6 +2,7 @@ package zajecia12;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("la la");
+        System.out.println("la la");
     }
 }
