d30ba46b4b6e7e1bc6492ed029277192cee24f96
Jan <janek0619@mail.com>
2022-04-14 17:17:35 +0200

    feature|intefejsy funkcyjne

--- src/zajecia12/Main.java
+++ src/zajecia12/Main.java
@@ -1,10 +1,23 @@
 package zajecia12;
 
+import java.util.Locale;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("la la");
-        System.out.println("la la");
-        System.out.println("Hello World");
+
+
+
+        Predicate<Integer> zwrocOsobePelnoletnia = (wiek) -> wiek >= 18;
+        System.out.println(zwrocOsobePelnoletnia.test(18));
+
+        Function<String,String> zamienNaDuzeLitery = (napis) -> napis.toUpperCase(Locale.ROOT);
+        System.out.println(zamienNaDuzeLitery.apply("abcd"));
+
+        
+
 
     }
 }
